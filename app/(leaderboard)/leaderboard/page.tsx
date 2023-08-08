271e113d8a7f2f74ebb50a7bb7806c16c3c0c2db
afdon <53676753+afdon@users.noreply.github.com>
2023-08-08 17:01:23 -0400

    fixed error

---
+++ app/(leaderboard)/leaderboard/page.tsx
@@ -0,0 +1,14 @@
+import { Card } from "@/components/ui/card"
+
+const Leaderboard = () => {
+    return (
+        <>
+            <Card>
+                Leaderboard
+            </Card>
+        </>
+
+    );
+}
+
+export default Leaderboard;

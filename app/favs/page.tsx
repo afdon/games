6d3b444964bb6e80eb2d72539508ca3b8672a68e
afdon <53676753+afdon@users.noreply.github.com>
2023-08-08 18:51:33 -0400

    tweaked margins

--- app/favs/page.tsx
+++ app/favs/page.tsx
@@ -33,25 +33,26 @@ const LikedGames = async () => {
                     gap-x-5
                     "
                     >
-                        <div className="
+                        {/* <div className="
                         relative
                         h-32
                         w-32
                         lg:h-44
                         lg:w-44
                         ">
-                            {/* <Image 
+                            <Image 
                             fill
                             alt="Playlist"
                             className="object-cover"
                             src="/images/astronaut-riding-rocket.jpg"
-                            /> */}
-                        </div>
+                            />
+                        </div> */}
+
                         <div className="
                         flex
                         flex-col
                         gap-y-2
-                        mt-4
+                        m-4
                         md:mt-0
                         ">
                             <p className="hidden md:block font-sem

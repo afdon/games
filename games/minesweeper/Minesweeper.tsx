03e13dcab710c5c5c9996c49ff510290bd754ecf
Robbie Hirsch <4796485+robbiehirsch@users.noreply.github.com>
2023-08-08 18:12:48 -0700

    fixed bug with mine adjacency counter incrementing every render

--- games/minesweeper/Minesweeper.tsx
+++ games/minesweeper/Minesweeper.tsx
@@ -2,37 +2,37 @@
 
 import { useState } from "react";
 import {
-  settings,
-  changeSettings,
-  reinit,
-  findConnectedCellsToReveal,
-  getDisplayValues,
-  initialize,
+    settings,
+    changeSettings,
+    reinit,
+    findConnectedCellsToReveal,
+    getDisplayValues,
+    initialize,
 } from "./setup.js";
 import "./Minesweeper.css";
 
 import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
 } from "@/components/ui/popover"
 import { Button } from "@/components/ui/button"
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
 } from "@/components/ui/select"
 import { Separator } from "@/components/ui/separator";
 import { useRouter } from "next/navigation.js";
@@ -41,200 +41,180 @@ import { useRouter } from "next/navigation.js";
 
 
 export default function Game() {
+    const router = useRouter();
+    const [settings, setSettings] = useState({ mines: 25, rows: 16, cols: 30 });
+    const [gameState, setGameState] = useState("playing");
+    const [flagged, setFlagged] = useState(new Array(settings.mines).fill(false));
+    const [revealedCells, setRevealedCells] = useState(new Array(settings.rows * settings.cols).fill(false));
 
-  const [ settings, setSettings ] = useState({ mines: 25, rows: 16, cols: 30 });
+    // const longestSide = Math.max(settings.rows, settings.cols);
+    // const divided = 100 / longestSide;
 
-  const minesLeft = settings.mines - numFlagged
+    const total = settings.rows * settings.rows;
+    let numRevealed = revealedCells.filter((cell) => cell === true).length;
+    let numFlagged = flagged.filter((cell) => cell === true).length;
+    const minesLeft = settings.mines - numFlagged
 
-  const [gameState, setGameState] = useState("playing");
+    const toggleFlag = (element: any, index: number) => {
+        element.preventDefault();
 
-  const [flagged, setFlagged] = useState(
-    new Array(settings.mines).fill(false)
-  );
+        if (revealedCells[index]) return;
 
-  const [revealedCells, setRevealedCells] = useState(
-    new Array(settings.rows * settings.cols).fill(false)
-  );
+        let flaggedCopy = [...flagged];
+        flaggedCopy[index] = !flaggedCopy[index];
+        setFlagged(flaggedCopy);
 
-  const toggleFlag = (element: any, index: number) => {
-    element.preventDefault();
+        checkWin();
+    };
 
-    if (revealedCells[index]) return;
+    const handleCellClick = (index: number) => {
+        let newRevCells;
 
-    let flaggedCopy = [...flagged];
-    flaggedCopy[index] = !flaggedCopy[index];
-    setFlagged(flaggedCopy);
+        if (board[index] >= 10) {
+            setGameState("lost");
+            newRevCells = new Array(settings.rows * settings.cols).fill(true);
+        } else {
+            newRevCells = [...revealedCells];
+            const cellsToReveal = findConnectedCellsToReveal(index, board);
+            cellsToReveal.forEach((c) => {
+                newRevCells[c] = true;
+            });
+            newRevCells[index] = true;
+        }
 
-    checkWin();
-  };
+        setRevealedCells(newRevCells);
+    };
 
-  const handleCellClick = (index: number) => {
+    let checkWin = () => {
+        console.log("numRevealed", numRevealed);
+        console.log("numFlagged", numFlagged);
 
-    let newRevCells;
+        if (numRevealed + numFlagged >= total) {
+            setGameState("won");
+        }
+    };
 
-    if (board[index] >= 10) {
-      setGameState("lost");
-      newRevCells = new Array(settings.rows * settings.cols).fill(true);
-    } else {
-      newRevCells = [...revealedCells];
-      const cellsToReveal = findConnectedCellsToReveal(index, board);
-      cellsToReveal.forEach((c) => {
-        newRevCells[c] = true;
-      });
-      newRevCells[index] = true;
+    const getProgress = () => {
+        return total - revealedCells.length
     }
 
-    setRevealedCells(newRevCells);
-  };
-
-  const total = settings.rows * settings.rows;
-  let numRevealed = revealedCells.filter((cell) => cell === true).length;
-  let numFlagged = flagged.filter((cell) => cell === true).length;
-
-  let checkWin = () => {
-    console.log("numRevealed", numRevealed);
-    console.log("numFlagged", numFlagged);
-
-    if (numRevealed + numFlagged >= total) {
-      setGameState("won");
+    const handleReset = () => {
+        // if value === "easy" 
+        // if value === "medium"
+        // if value === "hard"
     }
-  };
 
-  const longestSide = Math.max(settings.rows, settings.cols);
-  const divided = 100 / longestSide;
+    const [board, setBoard] = useState(initialize(settings));
+    const [display, setDisplay] = useState(getDisplayValues(board));
 
+    return (
+        <>
+            {gameState === "lost" && <p>You lost!</p>}
+            {gameState === "won" && <p>You won!</p>}
 
-  const getProgress = () => {
-    return total - revealedCells.length
-  }
+            {/* <div>Mines Left: {SETTINGS.numMines - numFlagged}</div> */}
 
-  const router = useRouter();
-  
-  const handleReset = () => {
-    // if value === "easy" 
-    // if value === "medium"
-    // if value === "hard"
-  }
-  
-    let board = initialize(settings);
-    let display = getDisplayValues(board);
-
-  return (
-    <> 
-      {gameState === "lost" && <p>You lost!</p>}
-      {gameState === "won" && <p>You won!</p>}
-
-      {/* <div>Mines Left: {SETTINGS.numMines - numFlagged}</div> */}
-
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button variant="outline"><div>Mines Left to Flag: {minesLeft}</div></Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-100">
-          <Card className="w-[350px]">
-            <CardHeader>
-              {/* <CardTitle className="text-sm">Settings</CardTitle> */}
-              <CardDescription className="text-bold">Customize your game.</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form>
-                <div className="grid w-full items-center gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    {/* <Label htmlFor="mines">Mines</Label>
+            <Popover>
+                <PopoverTrigger asChild>
+                    <Button variant="outline"><div>Mines Left to Flag: {minesLeft}</div></Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-100">
+                    <Card className="w-[350px]">
+                        <CardHeader>
+                            {/* <CardTitle className="text-sm">Settings</CardTitle> */}
+                            <CardDescription className="text-bold">Customize your game.</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <form>
+                                <div className="grid w-full items-center gap-4">
+                                    <div className="flex flex-col space-y-1.5">
+                                        {/* <Label htmlFor="mines">Mines</Label>
                     <Separator /> */}
-                    <Input id="mines" placeholder="12" className="w-[50px]"/>
-                  </div>
-                  <div className="flex flex-col space-y-1.5">
-                    {/* <Label htmlFor="difficulty">Level</Label>
+                                        <Input id="mines" placeholder="12" className="w-[50px]" />
+                                    </div>
+                                    <div className="flex flex-col space-y-1.5">
+                                        {/* <Label htmlFor="difficulty">Level</Label>
                     <Separator /> */}
-                    <Select>
-                      <SelectTrigger id="difficulty">
-                        <SelectValue placeholder="Difficulty" />
-                      </SelectTrigger>
-                      <SelectContent position="popper">
-                        <SelectItem value="easy">Easy</SelectItem>
-                        <SelectItem value="standard">Standard</SelectItem>
-                        <SelectItem value="medium">Medium</SelectItem>
-                        <SelectItem value="hard">Hard</SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                </div>
-              </form>
-            </CardContent>
-            <CardFooter className="flex justify-between">
-              <Button variant="outline">Cancel</Button>
-              <Button onClick={handleReset}>Go</Button>
-            </CardFooter>
-          </Card>
-        </PopoverContent>
-      </Popover>
-      
-      <Button variant="outline" onClick={() => router.refresh()}>Reset</Button>
+                                        <Select>
+                                            <SelectTrigger id="difficulty">
+                                                <SelectValue placeholder="Difficulty" />
+                                            </SelectTrigger>
+                                            <SelectContent position="popper">
+                                                <SelectItem value="easy">Easy</SelectItem>
+                                                <SelectItem value="standard">Standard</SelectItem>
+                                                <SelectItem value="medium">Medium</SelectItem>
+                                                <SelectItem value="hard">Hard</SelectItem>
+                                            </SelectContent>
+                                        </Select>
+                                    </div>
+                                </div>
+                            </form>
+                        </CardContent>
+                        <CardFooter className="flex justify-between">
+                            <Button variant="outline">Cancel</Button>
+                            <Button onClick={handleReset}>Go</Button>
+                        </CardFooter>
+                    </Card>
+                </PopoverContent>
+            </Popover>
 
-      <div className="p-4"></div>
+            <Button variant="outline" onClick={() => router.refresh()}>Reset</Button>
 
-      <div
-        className={`grid grid-rows-${settings.rows} grid-cols-${settings.cols}`}
-        style={{
-          display: "grid",
-          width: "100vh",
-          height: "auto",
-          gridTemplateRows: `repeat(${settings.rows},
-            calc(100vmin / ${Math.max(
-            settings.rows,
-            settings.cols
-          )}))
-            `,
-          gridTemplateColumns: `repeat(${settings.cols},
-            calc(100vmin / ${Math.max(
-            settings.rows,settings.cols
-          )}))            `,
-          overflow: "hidden",
-          margin: "auto",
-        }}
-      >
-        {display.map((element: any, index: number) => (
-          <GameCell
-            key={index}
-            index={index}
-            isRevealed={revealedCells[index]}
-            isFlagged={flagged[index]}
-            cellValue={element}
-            onClick={() => handleCellClick(index)}
-            onContextMenu={(event: any) => toggleFlag(event, index)}
-          />
-        ))}
-      </div>
-    </>
-  );
+            <div className="p-4"></div>
+
+            <div
+                className={`grid grid-rows-${settings.rows} grid-cols-${settings.cols}`}
+                style={{
+                    display: "grid",
+                    width: "100vh",
+                    height: "auto",
+                    gridTemplateRows: `repeat(${settings.rows}, calc(100vmin / ${Math.max(settings.rows, settings.cols)})) `,
+                    gridTemplateColumns: `repeat(${settings.cols}, calc(100vmin / ${Math.max(settings.rows, settings.cols)}))`,
+                    overflow: "hidden",
+                    margin: "auto",
+                }}
+            >
+                {display.map((element: any, index: number) => (
+                    <GameCell
+                        key={index}
+                        index={index}
+                        isRevealed={revealedCells[index]}
+                        isFlagged={flagged[index]}
+                        cellValue={element}
+                        onClick={() => handleCellClick(index)}
+                        onContextMenu={(event: any) => toggleFlag(event, index)}
+                    />
+                ))}
+            </div>
+        </>
+    );
 }
 
 const GameCell = (props: any) => {
-  let cn = "cell";
+    let cn = "cell";
 
-  if (props.isRevealed) {
-    cn += " revealed";
-  } else {
-    cn += " notrevealed";
-  }
+    if (props.isRevealed) {
+        cn += " revealed";
+    } else {
+        cn += " notrevealed";
+    }
 
-  if (props.isFlagged) {
-    cn += " flagged";
-  } else {
-    cn += " notflagged";
-  }
+    if (props.isFlagged) {
+        cn += " flagged";
+    } else {
+        cn += " notflagged";
+    }
 
-  return (
-    <div
-      key={props.index}
-      className={cn}
-      onClick={props.onClick}
-      onContextMenu={props.onContextMenu}
-    >
-      {props.isRevealed && props.cellValue}
-      {!props.isRevealed && props.isFlagged && "🚩"}
-      {/* {props.isRevealed && `${getDisplayValue(props.cellValue)}`} */}
-    </div>
-  );
+    return (
+        <div
+            key={props.index}
+            className={cn}
+            onClick={props.onClick}
+            onContextMenu={props.onContextMenu}
+        >
+            {props.isRevealed && props.cellValue}
+            {!props.isRevealed && props.isFlagged && "🚩"}
+            {/* {props.isRevealed && `${getDisplayValue(props.cellValue)}`} */}
+        </div>
+    );
 };
--- games/minesweeper/setup.js
+++ games/minesweeper/setup.js
@@ -1,28 +1,27 @@
-  
-  let board = [];
-  let mineIndices = [];
+
+let board = [];
+let mineIndices = [];
 
 export let settings = {
     mines: 30,
     rows: 16,
     cols: 30,
-  };
-  
-  // export const cellSize = `calc(100vmin / ${Math.max(SETTINGS.numRows, SETTINGS.numCols)})`;
-  export const cellSize = `${90 / (Math.max(settings.rows, settings.cols))})vmin`;
+};
+
+// export const cellSize = `calc(100vmin / ${Math.max(SETTINGS.numRows, SETTINGS.numCols)})`;
+export const cellSize = `${90 / (Math.max(settings.rows, settings.cols))})vmin`;
 
 
-  export const changeSettings = (m, r, c) => {
-    
+export const changeSettings = (m, r, c) => {
     return settings;
-  };
+};
 
-  export const reinit = (m, r, c) => {
+export const reinit = (m, r, c) => {
     changeSettings(m, r, c)
     board = initialize()
     display = getDisplayValues(b)
     return display;
-  }
+}
 
 
 // export const start = {
@@ -31,64 +30,64 @@ export let settings = {
 //   generateMineIndices: generateMineIndices,
 //   fillMines: fillMines,
 // }
-  
-  export const createBoard = (rows, cols) => {
+
+export const createBoard = (rows, cols) => {
     for (let i = 0; i < rows * cols; i++) {
-      board.push(0);
+        board.push(0);
     }
     return board;
-  };
-  
-  const cellIndex = (rowIndex, colIndex, rows, cols) => {
+};
+
+const cellIndex = (rowIndex, colIndex, rows, cols) => {
     if (rowIndex < 0 || rowIndex >= rows || colIndex < 0 || colIndex >= cols) {
-      return null;
+        return null;
     }
     const cellIdx = rowIndex * cols + colIndex;
     return cellIdx;
-  };
-  
-  export const getRandomIndex = (totalCells) => {
+};
+
+export const getRandomIndex = (totalCells) => {
     const index = Math.floor(Math.random() * totalCells);
     return index;
-  };
-  
-  export const generateMineIndices = (numberOfMines, rows, cols) => {
+};
+
+export const generateMineIndices = (numberOfMines, rows, cols) => {
     let totalCells = rows * cols;
-  
+
     while (mineIndices.length < numberOfMines) {
-      let mineIdx;
-      mineIdx = getRandomIndex(totalCells);
-  
-      if (!mineIndices.includes(mineIdx)) {
-        mineIndices.push(mineIdx);
-      }
+        let mineIdx;
+        mineIdx = getRandomIndex(totalCells);
+
+        if (!mineIndices.includes(mineIdx)) {
+            mineIndices.push(mineIdx);
+        }
     }
-  
+
     return mineIndices;
-  };
-  
-  export const fillMines = (mineIndices, board) => {
+};
+
+export const fillMines = (mineIndices, board) => {
     for (let i = 0; i < mineIndices.length; i++) {
-      let mineIdx = mineIndices[i];
-      board[mineIdx] = 10;
-  
-      incrementNeighbors(mineIdx, settings.rows, settings.cols);
+        let mineIdx = mineIndices[i];
+        board[mineIdx] = 10;
+
+        incrementNeighbors(mineIdx, settings.rows, settings.cols);
     }
     return board;
-  };
-  
-  export const incrementNeighbors = (mineIdx, rows, cols) => {
+};
+
+export const incrementNeighbors = (mineIdx, rows, cols) => {
     let neighbors = getNeighborsIdx(mineIdx, rows, cols);
     // console.log("neighbors", neighbors);
     for (let i = 0; i < neighbors.length; i++) {
-      let idx = neighbors[i];
-      if (typeof board[idx] === "number") {
-        board[idx]++;
-      }
+        let idx = neighbors[i];
+        if (typeof board[idx] === "number") {
+            board[idx]++;
+        }
     }
-  };
-  
-  export const getNeighborsIdx = (index, rows, cols) => {
+};
+
+export const getNeighborsIdx = (index, rows, cols) => {
     const rowIdx = Math.floor(index / cols);
     const colIdx = index % cols;
     let north = cellIndex(rowIdx - 1, colIdx, rows, cols);
@@ -99,22 +98,22 @@ export let settings = {
     let northwest = cellIndex(rowIdx - 1, colIdx - 1, rows, cols);
     let southeast = cellIndex(rowIdx + 1, colIdx + 1, rows, cols);
     let southwest = cellIndex(rowIdx + 1, colIdx - 1, rows, cols);
-  
+
     const neighbors = {
-      north: north,
-      east: east,
-      south: south,
-      west: west,
-      northeast: northeast,
-      northwest: northwest,
-      southeast: southeast,
-      southwest: southwest,
+        north: north,
+        east: east,
+        south: south,
+        west: west,
+        northeast: northeast,
+        northwest: northwest,
+        southeast: southeast,
+        southwest: southwest,
     };
     //   return neighbors;
     return Object.values(neighbors);
-  };
-  
-  export const initialize = (s) => {
+};
+
+export const initialize = (s) => {
     let board = createBoard(s.rows, s.cols);
     let mineIndices = generateMineIndices(
         s.mines,
@@ -123,41 +122,41 @@ export let settings = {
     );
     board = fillMines(mineIndices, board);
     return board;
-  };
-  
-  export const getDisplayValues = (array) => {
+};
+
+export const getDisplayValues = (array) => {
     return array.map((element) => {
-      if (element === 0) { return "" } // blanks
-      if (element >= 10) { return "💣" } // 💣 🍘
-      if (element > 0 && element < 10) { return element } // adjacents
-      // if (element < 0) { return "" } // hidden
-      return element; // return the element as is if none of the conditions match
+        if (element === 0) { return "" } // blanks
+        if (element >= 10) { return "💣" } // 💣 🍘
+        if (element > 0 && element < 10) { return element } // adjacents
+        // if (element < 0) { return "" } // hidden
+        return element; // return the element as is if none of the conditions match
     });
-  }
-  
-  export const findConnectedCellsToReveal = (index, board) => {
-      let queue = [index];
-      let visited = [];
-      let cellsToReveal = [];
-  
-      let counter = 0;
-  
-      while (queue.length > 0 && counter < settings.rows * settings.cols) {
-          counter++;
-          let curCell = queue.shift();
-  
-          if (board[curCell] === 0) {
-              let neighbors = getNeighborsIdx(curCell, settings.rows, settings.cols).slice(0, 4);
-              neighbors.forEach(n => {
-                  cellsToReveal.push(n);
-  
-                  if (n !== null && !visited.includes(n) && board[n] === 0) {
-                      visited.push(n);
-                      queue.push(n);
-                  }
-              })
-          }
-      }
-  
-      return cellsToReveal;
-  }
+}
+
+export const findConnectedCellsToReveal = (index, board) => {
+    let queue = [index];
+    let visited = [];
+    let cellsToReveal = [];
+
+    let counter = 0;
+
+    while (queue.length > 0 && counter < settings.rows * settings.cols) {
+        counter++;
+        let curCell = queue.shift();
+
+        if (board[curCell] === 0) {
+            let neighbors = getNeighborsIdx(curCell, settings.rows, settings.cols).slice(0, 4);
+            neighbors.forEach(n => {
+                cellsToReveal.push(n);
+
+                if (n !== null && !visited.includes(n) && board[n] === 0) {
+                    visited.push(n);
+                    queue.push(n);
+                }
+            })
+        }
+    }
+
+    return cellsToReveal;
+}
